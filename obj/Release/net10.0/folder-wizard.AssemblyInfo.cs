1262c9c22e90b6ad99d1e0b1141ff9016a2c5c74
Stiven Páez <stevenpaezarias1@hotmail.com>
2026-02-03 21:04:57 -0500

    feat: add validation to windows SO

--- obj/Release/net10.0/folder-wizard.AssemblyInfo.cs
+++ obj/Release/net10.0/folder-wizard.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("folder-wizard")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2392ebeb904bc98e8c68531de2706fb49824261a")]
 [assembly: System.Reflection.AssemblyProductAttribute("folder-wizard")]
